433794871e0a999c7e32999e088c394fffe8aee5
Luke J. Basso <lukejbass@gmail.com>
2024-01-27 17:57:06 -0800

    Final section

---
+++ src/components/UsersList.js
@@ -0,0 +1,44 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { fetchUsers, addUser } from '../store';
+import Button from './Button';
+import Skeleton from './Skeleton';
+import UsersListItem from './UsersListItem.js';
+import { useThunk } from '../hooks/useThunk';
+
+function UsersList() {
+    const [doFetchUsers, isFetchingUsers, fetchingUsersError] = useThunk(fetchUsers);
+    const [doAddUser, isAddingUser, addingUserError] = useThunk(addUser);
+    const { data } = useSelector((state) => state.users);
+
+    useEffect(() => {
+        doFetchUsers();
+    }, [doFetchUsers]);
+
+    let content;
+    if (isFetchingUsers) content = <Skeleton times={6} className='h-10 w-full' />;
+    else if (fetchingUsersError) content = <div>Error...</div>;
+    else {
+        content = data.map((user) => (
+            <UsersListItem key={user.id} user={user} />
+        ))
+    }
+
+    return (
+        <div>
+            <div className='flex justify-between items-center m-3'>
+                <h1 className='m-2 text-xl'>Users</h1>
+                <Button 
+                    loading={isAddingUser} 
+                    onClick={doAddUser}
+                >
+                    + Add User
+                </Button>
+                {addingUserError && 'Error Creating User'}
+            </div>
+            {content}
+        </div>
+    );
+}
+
+export default UsersList;
---
+++ src/components/UsersListItem.js
@@ -0,0 +1,32 @@
+import { GoTrashcan } from 'react-icons/go';
+import Button from './Button';
+import ExpandablePanel from './ExpandablePanel';
+import AlbumsList from './AlbumsList';
+import { removeUser } from '../store';
+import { useThunk } from '../hooks/useThunk';
+
+function UsersListItem({ user }) {
+    const [doRemoveUser, isLoading, error] = useThunk(removeUser);
+
+    const header = (
+        <>
+            <Button 
+                className='mr-3'
+                loading={isLoading} 
+                onClick={() => doRemoveUser(user)}
+            >
+                <GoTrashcan />
+            </Button>
+            {error && <div>Error Deleting User</div>}
+            {user.name}
+        </>
+    );
+
+    return (
+        <ExpandablePanel header={header}>
+            <AlbumsList user={user} />
+        </ExpandablePanel>
+    );
+}
+
+export default UsersListItem;
